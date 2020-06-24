7981b29475ff21e5df6a2c054d0c9a7370761ad4
wasimiqbal1 <wasim_iqbal@msn.com>
2020-06-25 03:00:47 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return ( < div > Hello World React < /div> );
+    return ( < div > Hello World React - app < /div> );
     }
 
     export default App;
